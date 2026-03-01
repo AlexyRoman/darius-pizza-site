a8344763996887ef59a65efa271c2fef654c4d61
AlexyRoman <alexyroman.dev@gmail.com>
2026-03-01 17:35:56 +0000

    fix: playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,13 +2,13 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests/e2e',
-  testIgnore: process.env['CI'] ? ['**/*'] : [],
+  testIgnore: [],
   fullyParallel: true,
   forbidOnly: !!process.env['CI'],
   retries: process.env['CI'] ? 2 : 0,
   reporter: 'html',
   use: {
-    baseURL: 'http://localhost:3001',
+    baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'off',
